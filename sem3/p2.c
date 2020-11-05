15544d16d16058620940e55b99e0918e67a98c85
Damjanp06 <damjanp06@gmail.com>
2020-11-05 23:46:45 +0100

    www

--- sem3/p2.c
+++ sem3/p2.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-    printf("Hello world!");
+    char n[100]; //zarezerwuj pamiec na sto znakow
+    int i;
+    strcpy(n, "Ala");
+    for(i=0;i<3;i++){
+    printf("\n[%d] %c, %d\n", i, n[i], n[1]);
+    }
+    return 0; 
 }
